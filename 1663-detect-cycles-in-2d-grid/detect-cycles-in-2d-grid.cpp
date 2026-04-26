0dc547634d52c6399d4320fc2036cd5f0e006992
Tarun Pratap Singh <109531127+TarunCoder123@users.noreply.github.com>
2026-04-26 17:46:11 +0530

    Time: 381 ms (15.69%) | Memory: 99.8 MB (35.57%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,59 +1,49 @@
-class UnionFind {
-public:
-    vector<int> parent;
-    vector<int> size;
-    int n;
-    int setCount;
-
-public:
-    UnionFind(int _n) : n(_n), setCount(_n), parent(_n), size(_n, 1) {
-        iota(parent.begin(), parent.end(), 0);
-    }
-
-    int findset(int x) {
-        return parent[x] == x ? x : parent[x] = findset(parent[x]);
-    }
-
-    void unite(int x, int y) {
-        if (size[x] < size[y]) {
-            swap(x, y);
-        }
-        parent[y] = x;
-        size[x] += size[y];
-        --setCount;
-    }
-
-    bool findAndUnite(int x, int y) {
-        int parentX = findset(x);
-        int parentY = findset(y);
-        if (parentX != parentY) {
-            unite(parentX, parentY);
-            return true;
-        }
-        return false;
-    }
-};
-
 class Solution {
 public:
+    int m, n;
+    vector<vector<int>> visited;
+    
+    bool dfs(vector<vector<char>>& grid, int i, int j, int pi, int pj) {
+        visited[i][j] = 1;
+        
+        int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        
+        for (auto &d : dirs) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+            
+            if (ni < 0 || nj < 0 || ni >= m || nj >= n) continue;
+            
+            if (grid[ni][nj] != grid[i][j]) continue;
+            
+            // if not visited → go deeper
+            if (!visited[ni][nj]) {
+                if (dfs(grid, ni, nj, i, j)) return true;
+            } 
+            // if visited and NOT parent → cycle found
+            else if (ni != pi || nj != pj) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
     bool containsCycle(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        UnionFind uf(m * n);
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i > 0 && grid[i][j] == grid[i - 1][j]) {
-                    if (!uf.findAndUnite(i * n + j, (i - 1) * n + j)) {
-                        return true;
-                    }
-                }
-                if (j > 0 && grid[i][j] == grid[i][j - 1]) {
-                    if (!uf.findAndUnite(i * n + j, i * n + j - 1)) {
+        m = grid.size();
+        n = grid[0].size();
+        visited.assign(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    if (dfs(grid, i, j, -1, -1)) {
                         return true;
                     }
                 }
             }
         }
+        
         return false;
     }
 };

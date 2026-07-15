c36ba977d36d69604172a9c858892d5db5d2b4e6
Tarun Pratap Singh <109531127+TarunCoder123@users.noreply.github.com>
2026-07-15 16:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (39.24%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,33 +1,57 @@
 class Solution {
 public:
 
+// bool bfsColor(int node,int colorValue,vector<vector<int>> &graph,vector<int> &color){
+//   queue<int> q;
+//   q.push(node);
+//   color[node]=colorValue;
+//   while(!q.empty()){
+// 	  int node=q.front();
+// 	  q.pop();
+// 	  for(auto neigh:graph[node]){
+//         if(color[neigh]==-1){
+//            q.push(neigh);
+//            color[neigh]=!color[node];
+// 		}else if(color[neigh]==color[node])return false;
+// 	  }
+//   }
+//   return true;
+// }
 bool bfsColor(int node,int colorValue,vector<vector<int>> &graph,vector<int> &color){
-  queue<int> q;
-  q.push(node);
-  color[node]=colorValue;
-  while(!q.empty()){
-	  int node=q.front();
-	  q.pop();
-	  for(auto neigh:graph[node]){
-        if(color[neigh]==-1){
-           q.push(neigh);
-           color[neigh]=!color[node];
-		}else if(color[neigh]==color[node])return false;
-	  }
-  }
-  return true;
+    queue<int> q;
+    q.push(node);
+    color[node]=colorValue;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        for(auto neigh:graph[node]){
+            if(color[neigh]==-1){
+                q.push(neigh);
+                color[neigh]=!color[node];
+            }else if(color[neigh]==color[node])return false;
+        }
+    }
+    return true;
 }
 
 bool isBipartite(vector<vector<int>>& graph) {
-     int n=graph.size();
-	 vector<int> color(n,-1);
-	 for(int i=0;i<n;i++){
-      if(color[i]==-1){
-          if(!bfsColor(i,0,graph,color))return false;
-	    }
-	 }
+    //  int n=graph.size();
+	//  vector<int> color(n,-1);
+	//  for(int i=0;i<n;i++){
+    //   if(color[i]==-1){
+    //       if(!bfsColor(i,0,graph,color))return false;
+	//     }
+	//  }
 
-	 return true;
+	//  return true;
+    int n=graph.size();
+    vector<int> color(n,-1);
+    for(int i=0;i<n;i++){
+        if(color[i]==-1){
+            if(!bfsColor(i,0,graph,color))return false;
+        }
+    }
+    return true;
 }
 
 };

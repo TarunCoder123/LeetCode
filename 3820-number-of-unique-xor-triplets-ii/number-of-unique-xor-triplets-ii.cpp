75ce83d86e15099fe07b8c2568d4c2c191e87282
Tarun Pratap Singh <109531127+TarunCoder123@users.noreply.github.com>
2026-07-24 11:08:09 +0530

    Time: 458 ms (79.21%) | Memory: 48.4 MB (64.61%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    static const int MAXX = 2048;
-
     int uniqueXorTriplets(vector<int>& nums) {
-        vector<vector<int>> dp(4, vector<int>(MAXX, 0));
-        dp[0][0] = 1;
-
-        for (int x : nums) {
-            auto ndp = dp;
-
-            for (int cnt = 0; cnt < 3; cnt++) {
-                for (int xr = 0; xr < MAXX; xr++) {
-                    if (!dp[cnt][xr]) continue;
-
-                    // choose current element once
-                    ndp[cnt + 1][xr ^ x] = 1;
-
-                    // choose it twice
-                    if (cnt + 2 <= 3)
-                        ndp[cnt + 2][xr] = 1;
-
-                    // choose it three times
-                    if (cnt + 3 <= 3)
-                        ndp[cnt + 3][xr ^ x] = 1;
-                }
+        int m=0;
+        for(int v:nums){
+            m=max(m,v);
+        }
+        int u=1;
+        while(u<=m){
+            u<<=1;
+        }
+        vector<int> s(u);
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                s[nums[i]^nums[j]]=1;
             }
-
-            dp = move(ndp);
         }
 
-        int ans = 0;
-        for (int xr = 0; xr < MAXX; xr++)
-            ans += dp[3][xr];
-
+        vector<int> t(u);
+        for(int x=0;x<u;x++){
+            if(!s[x])continue;
+            for(int v:nums)t[x^v]=1;
+        }
+        int ans=0;
+        for(int x=0;x<u;x++){
+           if(t[x]){
+            ans++;
+           } 
+        }
         return ans;
     }
 };
